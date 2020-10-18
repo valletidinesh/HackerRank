5a171caada0d7a0c6a359124672e564423e08e71
DINESH VALLETI <52668382+valletidinesh@users.noreply.github.com>
2020-10-18 12:26:58 +0530

    Create LibraryFine.py

---
+++ LibraryFine.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the libraryFine function below.
+def libraryFine(d1, m1, y1, d2, m2, y2):
+    if (d2>=d1 and m2>=m1 and y2>=y1) or (m2>m1 and y2>=y1) or (y2>y1):
+        fine=0
+    elif d2<d1 and m2==m1 and y2>=y1:
+        fine=15*abs(d2-d1)
+    elif m2<m1 and y2==y1:
+        fine=500*abs(m2-m1)
+    else:
+        fine=10000
+    return fine
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    d1M1Y1 = input().split()
+
+    d1 = int(d1M1Y1[0])
+
+    m1 = int(d1M1Y1[1])
+
+    y1 = int(d1M1Y1[2])
+
+    d2M2Y2 = input().split()
+
+    d2 = int(d2M2Y2[0])
+
+    m2 = int(d2M2Y2[1])
+
+    y2 = int(d2M2Y2[2])
+
+    result = libraryFine(d1, m1, y1, d2, m2, y2)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
